cc738a5ab16b4ea3359f607a4bca7def1278d95b
fducau <fnd212@nyu.edu>
2017-02-27 12:18:51 -0500

    Adding initial files

---
+++ bigram.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: bigram <file>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    lines = sc.textFile(sys.argv[1], 1)
+    sentences = lines.glom() \
+                  .map(lambda x: " ".join(x)) \
+                  .flatMap(lambda x: x.split("."))
+
+    #Your code goes here
+
+
+
+
+    sc.stop()
